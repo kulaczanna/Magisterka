f6a5dacd4e9e9785611fbdcf7ff030be7c0ad7bc
Anna Kułacz <43969613+kulaczanna@users.noreply.github.com>
2020-08-27 20:38:01 +0200

    końcówka v32 poprawki od 28 i czesc 9 rozdziału 27.08.2020

---
+++ Aplikacja - Wszystkie pliki/model_Isaeva_Osipov.m
@@ -0,0 +1,159 @@
+function rownania = model_de_Pillis(t, x)
+
+T = x(1);
+L = x(2);
+M = x(3);
+I = x(4);
+I_alfa = x(5);
+liczba_dni_w_cyklu = x(6);
+grupa = x(7);
+metoda_leczenia = x(8);
+N = x(9);
+
+V_M = 0;
+V_I = 0;
+V_Ialfa = 0;
+
+switch grupa
+    case 1
+        % ustawienie parametrów I
+        a = 0.13;
+        b = 3e-10;
+        c_CTL = 4.4e-9;
+        d = 7.3e6;
+        e = 9.9e-9;
+        f = 0.33;
+        g = 1.6e7;
+        jj = 3.3e-9;
+        k = 1.8e-8;
+        l = 3e6;
+        p = 6.4;
+        q = 1.7;
+        
+    case 2
+        % ustawienie parametrów II
+        a = 0.13;
+        b = 3e-10;
+        c_CTL = 3.3e-9;
+        d = 7.3e6;
+        e = 9.6e-9;
+        f = 0.33;
+        g = 1.4e7;
+        jj = 2.9e-9;
+        k = 1.5e-8;
+        l = 3e6;
+        p = 6.4;
+        q = 1.7;
+        
+    case 3
+        % ustawienie parametrów III
+        a = 0.13;
+        b = 3e-10;
+        c_CTL = 5.5e-9;
+        d = 7.3e6;
+        e = 1e-8;
+        f = 0.33;
+        g = 2.4e7;
+        jj = 3.7e-9;
+        k = 2.1e-8;
+        l = 3e6;
+        p = 6.4;
+        q = 1.7;
+end
+
+% parametry dla chemioterapii
+    K_T = 0.9;
+    K_L = 0.6;
+    K_N = 0.6;
+
+    if(metoda_leczenia == 1)
+    % funkcja stężenia cytostatyku
+        if(t >= 0 && t <= 1 || t >= liczba_dni_w_cyklu && t <= liczba_dni_w_cyklu+1 ...
+                || t >= 2*liczba_dni_w_cyklu && t <= (2*liczba_dni_w_cyklu)+1 ...
+                || t >= 3*liczba_dni_w_cyklu && t <= (3*liczba_dni_w_cyklu)+1 ...
+                || t >= 4*liczba_dni_w_cyklu && t <= (4*liczba_dni_w_cyklu)+1 ...
+                || t >= 5*liczba_dni_w_cyklu && t <= (5*liczba_dni_w_cyklu)+1 ...
+                || t >= 6*liczba_dni_w_cyklu && t <= (6*liczba_dni_w_cyklu)+1 ...
+                || t >= 7*liczba_dni_w_cyklu && t <= (7*liczba_dni_w_cyklu)+1 ...
+                || t >= 8*liczba_dni_w_cyklu && t <= (8*liczba_dni_w_cyklu)+1)
+            V_M = 5;
+        end
+    end
+    
+    if(metoda_leczenia == 2)
+        % funkcja stężenia interleukiny-2
+        if(t >= 0 && t <= 4 || t >= liczba_dni_w_cyklu && t <= liczba_dni_w_cyklu+4 ...
+                || t >= 2*liczba_dni_w_cyklu && t <= (2*liczba_dni_w_cyklu)+4 ...
+                || t >= 3*liczba_dni_w_cyklu && t <= (3*liczba_dni_w_cyklu)+4)
+            V_I = 10e6;
+        end
+    end
+    
+    if(metoda_leczenia == 3)
+        % funkcja stężenia interleukiny-2
+        if(t >= 0 && t <= 4 || t >= liczba_dni_w_cyklu && t <= liczba_dni_w_cyklu+4 ...
+                || t >= 2*liczba_dni_w_cyklu && t <= (2*liczba_dni_w_cyklu)+4 ...
+                || t >= 3*liczba_dni_w_cyklu && t <= (3*liczba_dni_w_cyklu)+4)
+            V_Ialfa = 5e6;
+            V_I = 10e6;
+        end
+    end
+    
+    if(metoda_leczenia == 4)
+        % 1 puls chemio i 1 puls immuno
+        if(t >= 0 && t <= 4)
+            V_M = 1;
+        elseif(t > 4 && t <= 8)
+                V_I = 10e6;
+        end
+    end
+    
+    if(metoda_leczenia == 5)
+        % 1 puls chemio i 1 puls immuno
+        if(t >= 0 && t <= 4)
+            V_I = 10e6;
+        elseif(t > 4 && t <= 8)
+            V_M = 1;
+        end
+    end
+        
+    if(metoda_leczenia == 6)
+        % funkcja stężenia interleukiny-2
+        if(t >= 0 && t <= 4)
+            V_M = 1;
+            V_I = 7e6;
+        end
+    end
+    
+%% model
+
+I_alfa0 = 1e7;
+I_0 = 2.4e7;
+
+c = c_CTL*(2 - (exp((-I_alfa)/I_alfa0)));
+KT = K_T*(2 - (exp((-I)/I_0)));
+KL = K_L*(2 - (exp((-I)/I_0)));
+KN = K_N*(2 - (exp((-I)/I_0)));
+
+ce = 6.41e-11;
+ef = 4.12e-2;
+gie = 1.25e-2;
+ha = 2.02e7;
+pe = 3.42e-6;
+r1 = 1.1e-7;
+uu = 3e-10;
+
+% równania modelu
+dTdt = - a*T*log((b*T)/a)  - (ce * N * T) - c*T*L - ...
+    (KT*(1 - (exp(-M)))*T);
+dNdt = - (ef * N) + (gie * ((T^2) / (ha + (T^2))) * N) - ...
+    (pe * N * T) - (KN * (1 - (exp(-M))) * N);
+dLdt = d + (e*L*I) - (f*L) - (KL*(1 - (exp(-M)))*L) + ...
+    (r1 * N * T) - (uu * N * (L^2));
+dMdt = V_M - (p*M);
+dIdt = V_I + ((g*T)/(T+l)) - (jj*L*I) - (k*T*I);
+dIalfadt = V_Ialfa - (q*I_alfa);
+
+rownania = [dTdt; dLdt; dMdt; dIdt; dIalfadt; 0; 0; 0; dNdt];
+end
+
---
+++ Aplikacja - wersja czysta/wyswietl_wykresy.m
@@ -0,0 +1,39 @@
+function [] = wyswietl_wykresy(t, y)
+
+figure
+semilogy(t, y(:,1), 'r');
+hold on
+semilogy(t, y(:,2), 'g');
+hold on
+semilogy(t, y(:,3), 'b');
+hold on
+semilogy(t, y(:,4), 'c');
+axis([0, 120, 10e-1, 10e12])
+% axis([0, 120, 0, 10e12])
+legend('Komórki nowotworowe', 'Komórki NK', 'Limfocyty T_{CD8+}', 'Kr¹¿¹ce limfocyty');
+title({'Zmiany w czasie liczby komórek badanych populacji';'dla wartoœci pocz¹tkowych:';'T(0) = 1,8*10^7, N(0) = 10^5, L(0) = 10^2, C(0) = 3,5*10^9'})
+ylabel('Liczba komórek [log]')
+xlabel('Czas [dni]')
+
+figure
+subplot(3, 1, 1)
+plot(t, y(:, 5), 'g');
+title('Stê¿enie cytostatyka')
+legend('Cytostatyk')
+ylabel('Stê¿enie')
+xlabel('Czas [dni]')
+hold on
+subplot(3, 1, 2)
+plot(t, y(:, 6), 'g');
+legend('IL-2')
+title('Stê¿enie IL-2')
+ylabel('Stê¿enie')
+xlabel('Czas [dni]')
+% hold on
+% subplot(3, 1, 3)
+% plot(t, y(:, 6), 'g');
+% legend('IL-2')
+% ylabel('Stê¿enie')
+% xlabel('Czas [dni]')
+
+end
